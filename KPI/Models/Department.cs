146e12ec44fc3e990fb66bd3a5fc07045dc3711f
jiayushd <38585419+jiayushd@users.noreply.github.com>
2019-08-05 14:02:32 +0800

    以类库形式增加KPI模块

---
+++ KPI/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPI.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public string ParentID { get; set; }
+    }
+}
